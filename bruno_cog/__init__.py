606b455201e6afd5d33baf7494d21df9819b2129
brunood1 <ibruuno@outlook.com.br>
2024-01-20 21:12:04 -0300

    .

--- bruno_cog/__init__.py
+++ bruno_cog/__init__.py
@@ -1,7 +1,7 @@
-from .brunood_cog import length
+from .brunood_cog import Length
 
 __red_end_user_data_statement__ = "No user data is stored by this cog."
 
 
 async def setup(bot):
-    await bot.add_cog(length(bot))
+    await bot.add_cog(Length(bot))
