6aabe80b180bf5c8f42319a67b3e57952cf8e9c8
brunood1 <ibruuno@outlook.com.br>
2024-11-24 16:06:12 -0300

    config2

--- storehouse_manager/storehouse_manager.py
+++ storehouse_manager/storehouse_manager.py
@@ -52,7 +52,7 @@ class Storehouse(commands.Cog):
         self.config = Config.get_conf(self, identifier=143234942894832, force_registration=True)
         self.config.register_guild(
             storehouse_category_id=None,
-            live_category_id=None,
+            opened_category_id=None,
         )
 
     
@@ -64,6 +64,41 @@ class Storehouse(commands.Cog):
         return notice
     
     # Commands                
+    @commands.group(name="storehouse_config", invoke_without_command=True)
+    @commands.guild_only()
+    @commands.admin_or_permissions(manage_channels=True)
+    async def _storehouse_config(
+        self,
+        ctx: commands.Context,
+    ):
+        await ctx.send_help()
+        
+        
+    @_storehouse_config.command(name="storehouse")  
+    @commands.guild_only()
+    @commands.admin_or_permissions(manage_channels=True)
+    async def set_storehouse_category(
+        self,
+        ctx: commands.Context,
+        category: discord.CategoryChannel
+    ):
+        gld = ctx.guild
+        await self.config.guild(gld).storehouse_category_id.set(category.id)
+        await ctx.tick()
+    
+    @_storehouse_config.command(name="opened")  
+    @commands.guild_only()
+    @commands.admin_or_permissions(manage_channels=True)
+    async def set_opened_category(
+        self,
+        ctx: commands.Context,
+        category: discord.CategoryChannel
+    ):
+        gld = ctx.guild
+        await self.config.guild(gld).opened_category_id.set(category.id)
+        await ctx.tick()
+    
+    # Commands                
     @commands.command()  
     @commands.guild_only()
     @commands.admin_or_permissions(manage_channels=True)
@@ -73,8 +108,12 @@ class Storehouse(commands.Cog):
         channel: discord.TextChannel | discord.Thread):
         """Adds or removes :red_circle: from a channel name"""
         
-        STOREHOUSE = discord.utils.get(channel.guild.categories, id=1198407644021522452) 
-        OPENED_CHANNELS = discord.utils.get(channel.guild.categories, id=1198634992796975115)
+        gld = ctx.guild
+        storehouse_id = await self.config.guild(gld).storehouse_category_id()
+        opened_id = await self.config.guild(gld).opened_category_id()
+        
+        storehouse_cat = discord.utils.get(channel.guild.categories, id=storehouse_id) 
+        opened_cat = discord.utils.get(channel.guild.categories, id=opened_id)
         
         flag_emoji = "".join(c for c in channel.name if "🇦" <= c <= "🇿")
         
@@ -83,11 +122,11 @@ class Storehouse(commands.Cog):
         
         is_country = False if flag_emoji == "" else True
         
-        if channel in STOREHOUSE.channels:
+        if channel in storehouse_cat.channels:
             # Can't make storehouse channels live
             notice = self.CANT_GO_LIVE
         else: 
-            if is_country == True and channel in OPENED_CHANNELS.channels:
+            if is_country == True and channel in opened_cat.channels:
                 # Channel needs to be a country channel in the national category
                 
                 # Get country name
@@ -101,7 +140,7 @@ class Storehouse(commands.Cog):
                     # Organize channels into RED and NON-RED (Necessary for sorting)
                     red_channels = []    
                     non_red_channels = []                
-                    for ch in OPENED_CHANNELS.channels:
+                    for ch in opened_cat.channels:
                         # Get country name
                         ch_flag_emoji = "".join(c for c in ch.name if "🇦" <= c <= "🇿")
                         ch_country_code = "".join(self.indicator_convert.get(c, c) for c in ch_flag_emoji.lower())
@@ -171,8 +210,12 @@ class Storehouse(commands.Cog):
         ):
         """Moves a channel to and from the storehouse"""
         
-        STOREHOUSE = discord.utils.get(channel.guild.categories, id=1198407644021522452) 
-        OPENED_CHANNELS = discord.utils.get(channel.guild.categories, id=1198634992796975115)
+        gld = ctx.guild
+        storehouse_id = await self.config.guild(gld).storehouse_category_id()
+        opened_id = await self.config.guild(gld).opened_category_id()
+        
+        storehouse_cat = discord.utils.get(channel.guild.categories, id=storehouse_id) 
+        opened_cat = discord.utils.get(channel.guild.categories, id=opened_id)
         
         flag_emoji = "".join(c for c in channel.name if "🇦" <= c <= "🇿")
         
@@ -189,16 +232,16 @@ class Storehouse(commands.Cog):
                 country_name = self.countries[country_code]
                 
                 if status == "open":
-                    if channel in OPENED_CHANNELS.channels:
+                    if channel in opened_cat.channels:
                         # Can't open a channel that is already open
                         notice = self.CHANNEL_OPENED.format(mention)
-                    elif channel in STOREHOUSE.channels:
+                    elif channel in storehouse_cat.channels:
                         # When we move a channel to the opened channels category, we want them to be ordered alphabetically
                         # So, we first need to check where we should place the channel
                         # For that we need to consider two things, the currently opened channels with and without a red circle separately
                         red_channels = []    
                         non_red_channels = []          
-                        for ch in OPENED_CHANNELS.channels:
+                        for ch in opened_cat.channels:
                             # Get country name
                             ch_flag_emoji = "".join(c for c in ch.name if "🇦" <= c <= "🇿")
                             ch_country_code = "".join(self.indicator_convert.get(c, c) for c in ch_flag_emoji.lower())
@@ -216,7 +259,7 @@ class Storehouse(commands.Cog):
                         
                         try:
                             # adds channel to the national category in the beginning and moves it to the index
-                            await channel.move(beginning=True, offset=index, category=OPENED_CHANNELS, sync_permissions=True)
+                            await channel.move(beginning=True, offset=index, category=opened_cat, sync_permissions=True)
                         except discord.Forbidden:  # Manage channel perms required.
                             notice = self.channel_permission_error(channel, mention)
                         else:
@@ -224,16 +267,16 @@ class Storehouse(commands.Cog):
                     else:
                         notice = self.CANT_OPEN
                 elif status == "close":
-                    if channel in STOREHOUSE.channels:
+                    if channel in storehouse_cat.channels:
                         notice = self.CHANNEL_CLOSED
-                    elif channel in OPENED_CHANNELS.channels:
+                    elif channel in opened_cat.channels:
                         # First thing is to check if the channel we're trying to close has a red circle
                         if channel_name.startswith(self.LIVE_INDICATOR):
                             notice = self.CANT_CLOSE_LIVE
                         else:
                             # When we close a channel we need check its position in the storehouse
                             storehouse_channels = []
-                            for ch in STOREHOUSE.channels:
+                            for ch in storehouse_cat.channels:
                                 # Get country name
                                 ch_flag_emoji = "".join(c for c in ch.name if "🇦" <= c <= "🇿")
                                 if ch_flag_emoji != "":
@@ -247,7 +290,7 @@ class Storehouse(commands.Cog):
                             
                             try:
                                 # Moves category
-                                await channel.move(beginning=True, offset=index, category=STOREHOUSE, sync_permissions=True)
+                                await channel.move(beginning=True, offset=index, category=storehouse_cat, sync_permissions=True)
                             except discord.Forbidden:  # Manage channel perms required.
                                 notice = self.channel_permission_error(channel, mention)
                             else:
